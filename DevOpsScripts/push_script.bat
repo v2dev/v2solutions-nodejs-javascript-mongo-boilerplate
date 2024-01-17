b08c43b9a2b1f3c5094acfc11a8a02cbb9ed2e1d
sagarthoratv2 <sagar.thorat@v2solutions.com>
2024-01-17 19:24:02 +0530

    Adding push image code

--- DevOpsScripts/push_script.bat
+++ DevOpsScripts/push_script.bat
@@ -3,4 +3,4 @@
 set build_number=%1
 
 docker tag nodejs:%build_number% v2devops/v2solutions-nodejs-boilerplate/nodejs:%build_number%
-docker push v2devops/v2solutions-nodejs-boilerplate/nodejs
+docker push v2devops/v2solutions-nodejs-boilerplate/nodejs:%build_number%
