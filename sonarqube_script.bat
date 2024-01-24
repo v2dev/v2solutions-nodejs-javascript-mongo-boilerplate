e6630a0718b0bd732dea7d3fc28629bdf58117bf
sagarthoratv2 <sagar.thorat@v2solutions.com>
2024-01-24 22:40:12 +0530

    Adding helm chart and pushing to docker hub

--- sonarqube_script.bat
+++ sonarqube_script.bat
@@ -1,5 +1,6 @@
 @echo off
 set SCANNER_HOME=%1
 set PROJECT_KEY=%2
-%SCANNER_HOME%\bin\sonar-scanner -Dsonar.login=%SONARQUBE_CREDENTIALS% -Dsonar.projectKey=%PROJECT_KEY%
-%SCANNER_HOME%\bin\sonar-scanner.bat -D"sonar.projectKey=Nodejs" -D"sonar.sources=." -D"sonar.host.url=${SONARQUBE_SERVER}" -D"sonar.token=${SCAN_TOKEN}"
+@REM %SCANNER_HOME%\bin\sonar-scanner -Dsonar.login=%SONARQUBE_CREDENTIALS% -Dsonar.projectKey=%PROJECT_KEY%
+@REM %SCANNER_HOME%\bin\sonar-scanner.bat -D"sonar.projectKey=Nodejs" -D"sonar.sources=." -D"sonar.host.url=${SONARQUBE_SERVER}" -D"sonar.token=${SCAN_TOKEN}"
+%SCANNER_HOME%\bin\sonar-scanner -D"sonar.projectKey=%PROJECT_KEY%" -D"sonar.sources=." -D"sonar.host.url=%SONARQUBE_SERVER%" -D"sonar.login=%SONARQUBE_CREDENTIALS%" -D"sonar.token=%SCAN_TOKEN%"
